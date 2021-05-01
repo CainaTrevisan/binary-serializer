a2bf1e1073927f267d7cff7e686e6e216d4b6f44
Cainã Trevisan <cainact@gmail.com>
2021-05-01 16:49:11 -0300

    deserialize no longer needs to receive size and parameter order changed

--- serializer.h
+++ serializer.h
@@ -3,19 +3,23 @@
 
 typedef char byte;
 
-void serialize(const char * filename, void * data, int size) {
+void serialize(const char * filename, const size_t size, void * data) {
     FILE * file = fopen(filename, "w");
     byte * stream = (byte *) data; 
+    fwrite(&size, sizeof(size), 1, file);
     fwrite(stream, sizeof(byte), size, file);
     fclose(file);
 }
 
-void deserialize(const char * filename, void * data, int size) {
+void deserialize(const char * filename, void * data) {
     FILE * file = fopen(filename, "r");
     byte * stream = (byte *) data;
+    size_t size;
+    fread(&size, sizeof(size), 1, file);
     fread(stream, sizeof(byte), size, file);
     fclose(file); 
 }
+
 void file_delete(const char * filename) {
     if (remove(filename)) {
         fprintf(stderr, "File \"%s\" could not be deleted.", filename);
